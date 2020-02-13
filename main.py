3136393c4d28e3dbf5c77f3a53881488b5dbf303
s-maurice <mauricemscholtes@gmail.com>
2020-02-13 19:36:42 +0100

    added parsing for final attributes needed (action, action_target) changed origin to only get the origin player, target is handled by action and action_target parsing

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ log_data = open('logs/log_2469625.log', 'r')
 print(log_data)
 
 # first parse log data into df
-log_df = pd.DataFrame(columns=["time", "origin", "action_type", "action" "target", "details"])
+log_df = pd.DataFrame(columns=["time", "origin", "action_target", "action", "details"])
 
 test_logs = open('logs/testlines.log', 'r')
 
@@ -19,9 +19,9 @@ for line in test_logs:
     time = line[15:23]
 
     # get the details from in the brackets
-    details = re.findall("\((.+?)\)", line)
+    details = re.findall("\((.+?) \"(.+?)\"\)", line)
 
-    # get origin and target - check if non-player origin
+    # get origin - check if non-player origin
     # check for and find team
     team_search = re.findall('^Team "(Red|Blue)"', line[25:])
     if len(team_search) == 1:
@@ -29,14 +29,29 @@ for line in test_logs:
     elif line[25:30] == "World":  # check for and find world
         origin = "World"
     else:
-        # get players not in brackets
-        players = re.findall('\"(\w*<\d{4}><\[U:1:\d+\]><(Red|Blue)>)\"(?!\))', line)
-        origin = players[0]
-        if len(players) == 2:  # check if there's a target player
-            target = players[1]
-        else:
-            target = None
+        # get origin player
+        origin = re.findall(': "(\w*)<(\d{4})><\[(U:1:\d+?)\]><(Red|Blue)>"', line)[0]
 
+    # get action and action target
+    action, target = re.findall('(?:\"(?:\w*<\d{4}><\[U:1:\d+\]><(?:Red|Blue)>)\"|Team "(?:Red|Blue)"|World) ([\w ]+?) "(\S+?)"', line)[0]
+    # attempt to parse target as player
+    target_player_list = re.findall('(\w*)<(\d{4})><\[(U:1:\d+?)\]><(Red|Blue)>', target)
+    if len(target_player_list) > 0:
+        target = target_player_list[0]
+    # check if action is trigger type
+    if action == "triggered":
+        action, target = target, None
+        # attempt to parse triggered against
+        trigger_against_list = re.findall('(?:\"(?:\w*<\d{4}><\[U:1:\d+\]><(?:Red|Blue)>)\"|Team "(?:Red|Blue)"|World) triggered "(\S+?)" against "(\w*)<(\d{4})><\[(U:1:\d+?)\]><(Red|Blue)>"', line)
+        if len(trigger_against_list) > 0:
+            action = trigger_against_list[0][0]
+            target = trigger_against_list[0][1:]
+
+    # all details parsed - now get statistics
     print(line[25:-1])
+    print(time)
     print(origin)
+    print(action)
+    print(target)
+    print(details)
     print("\n")
