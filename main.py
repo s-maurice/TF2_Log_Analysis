7abb9016aed67b7718f14104d4742d6ce4985307
s-maurice <mauricemscholtes@gmail.com>
2020-03-05 18:30:42 +0100

    moved line parsing into func, refactored added checking for crossbow healing in HealStat creation added handling for ItemPickupStat creation added blank pointcaptured and captureblocked handling ifs

--- main.py
+++ main.py
@@ -7,32 +7,28 @@ from ActionsTriggers import ActionsTriggers, DetailAttributes
 
 # no support for tf2center logs, as extra plugins - only focus on pugchamp
 
-# pre-loop entry setup
-players_trigger_stat = PlayersTriggerStat()
-players_trigger_stat_list = [players_trigger_stat]
 
-test_logs = open('logs/testlines.log', 'r')
-for line in test_logs:
+def parseline(line_string):
     # possibly try to parse with single regex in the future
     # get the time
-    time = line[15:23]
+    time = line_string[15:23]
 
     # get the details from in the brackets
-    details = dict(re.findall("\((.+?) \"(.+?)\"\)", line))
+    details = dict(re.findall("\((.+?) \"(.+?)\"\)", line_string))
 
     # get origin - check if non-player origin
     # check for and find team
-    team_search = re.findall('^Team "(Red|Blue|RED|BLUE)"', line[25:])
+    team_search = re.findall('^Team "(Red|Blue|RED|BLUE)"', line_string[25:])
     if len(team_search) == 1:
         origin = team_search[0]
-    elif line[25:30] == "World":  # check for and find world
+    elif line_string[25:30] == "World":  # check for and find world
         origin = "World"
     else:
         # get origin player
-        origin = re.findall(': "([\w ]*)<(\d+)><\[(U:1:\d+?)\]><(Red|Blue|RED|BLUE)>"', line)[0]
+        origin = re.findall(': "([\w ]*)<(\d+)><\[(U:1:\d+?)\]><(Red|Blue|RED|BLUE)>"', line_string)[0]
 
     # get action and action target
-    action, target = re.findall('(?:\"(?:[\w ]*<\d+><\[U:1:\d+\]><(?:Red|Blue|RED|BLUE)>)\"|Team "(?:Red|Blue|RED|BLUE)"|World) ([\w ]+?) "(.+?)"', line)[0]
+    action, target = re.findall('(?:\"(?:[\w ]*<\d+><\[U:1:\d+\]><(?:Red|Blue|RED|BLUE)>)\"|Team "(?:Red|Blue|RED|BLUE)"|World) ([\w ]+?) "(.+?)"', line_string)[0]
     # attempt to parse target as player
     target_player_list = re.findall('([\w ]*)<(\d+)><\[(U:1:\d+?)\]><(Red|Blue|RED|BLUE)>', target)
     if len(target_player_list) > 0:
@@ -41,7 +37,7 @@ for line in test_logs:
     if action == "triggered":
         action, target = target, None
         # attempt to parse triggered against
-        trigger_against_list = re.findall('(?:\"(?:[\w ]*<\d+><\[U:1:\d+\]><(?:Red|Blue|RED|BLUE)>)\"|Team "(?:Red|Blue|RED|BLUE)"|World) triggered "(\S+?)" against "([\w ]*)<(\d+)><\[(U:1:\d+?)\]><(Red|Blue|RED|BLUE)>"', line)
+        trigger_against_list = re.findall('(?:\"(?:[\w ]*<\d+><\[U:1:\d+\]><(?:Red|Blue|RED|BLUE)>)\"|Team "(?:Red|Blue|RED|BLUE)"|World) triggered "(\S+?)" against "([\w ]*)<(\d+)><\[(U:1:\d+?)\]><(Red|Blue|RED|BLUE)>"', line_string)
         if len(trigger_against_list) > 0:
             action = trigger_against_list[0][0]
             target = trigger_against_list[0][1:]
@@ -50,8 +46,18 @@ for line in test_logs:
         # convert committed suicide with to committed suicide and then parse "with" in the same way for consistency
         if action == "committed suicide with":
             action = "committed suicide"
-        details.update(re.findall('(with) "(\w*?)"', line))
+        details.update(re.findall('(with) "(\w*?)"', line_string))
 
+    return time, origin, action, target, details
+
+# pre-loop entry setup
+players_trigger_stat = PlayersTriggerStat()
+players_trigger_stat_list = [players_trigger_stat]
+
+test_logs = open('logs/testlines.log', 'r')
+prev_line_stats = None
+for line_index, line in enumerate(test_logs):
+    time, origin, action, target, details = parseline(line)
     print(line[25:-1])
     print(time)
     print(origin)
@@ -74,6 +80,7 @@ for line in test_logs:
         # check if player is existing, if not create
         if spawner_trigger_stat is None:
             spawner_trigger_stat = PlayerTriggerStat(origin)
+            current_players_trigger_stat.add_player(spawner_trigger_stat)
         # set the class and the alive status
         spawner_trigger_stat.player_class = target
         spawner_trigger_stat.alive = True
@@ -98,6 +105,7 @@ for line in test_logs:
     elif action == ActionsTriggers.COMMITTED_SUICIDE:
         # copy of ActionsTriggers.KILLED, except attacker and victim are the same PlayerTriggerStat
         origin_trigger_stat = current_players_trigger_stat.get_player_by_steam_id(origin[2])
+        origin_trigger_stat.alive = False  # set player to dead
         # parse the details
         # add the position for this current tick, copy is already made for each tick
         origin_trigger_stat.set_position_by_string(details[DetailAttributes.ATTACKER_POSITION])
@@ -127,7 +135,10 @@ for line in test_logs:
         target_trigger_stat = current_players_trigger_stat.get_player_by_steam_id(target[2])
         healing = details.get(DetailAttributes.HEALING, 0)
         heal_stat = HealStat(origin_trigger_stat, target_trigger_stat, healing)
-        # TODO check if arrow
+        # check if previous line_string was arrow shot_hit
+        _, _, prev_action, _, prev_details = prev_line_stats
+        if prev_action == ActionsTriggers.SHOT_HIT and prev_details.get("weapon") == "crusaders_crossbow":
+            heal_stat.crossbow = True
 
     elif action == ActionsTriggers.DAMAGE:
         origin_trigger_stat = current_players_trigger_stat.get_player_by_steam_id(origin[2])
@@ -135,4 +146,17 @@ for line in test_logs:
         damage = details.get(DetailAttributes.REALDAMAGE, DetailAttributes.DAMAGE)
         weapon = details.get(DetailAttributes.WEAPON)
         damage_stat = DamageStat(origin_trigger_stat, target_trigger_stat, damage, weapon)
+
+    elif action == ActionsTriggers.POINTCAPTURED:
+        pass
+
+    elif action == ActionsTriggers.CAPTUREBLOCKED:
+        pass
+
+    elif action == ActionsTriggers.PICKED_UP_ITEM:
+        origin_trigger_stat = current_players_trigger_stat.get_player_by_steam_id(origin[2])
+        item_pickup_stat = ItemPickupStat(origin_trigger_stat, target)
+        item_pickup_stat.healing = details.get("healing", 0)
+
+    prev_line_stats = line_stats  # store the line as the prev line for the next pass
 
